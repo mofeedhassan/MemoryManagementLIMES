767b7c1f2bb530e561a8612409effed6b475724c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2016-11-18 11:50:02 +0100

    added JUnit test

---
+++ test/de/uni_leipzig/simba/memorymanagement/parallel/PLTSP/LTSPMainTest.java
@@ -0,0 +1,24 @@
+package de.uni_leipzig.simba.memorymanagement.parallel.PLTSP;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.uni_leipzig.simba.memorymanagement.testTSPCaching.TSPCachingTester;
+
+public class LTSPMainTest {
+
+	@Test
+	public void testRun1()
+	{
+		LTSPMain.main(new String[] {"run","PLTSP1KParametersRelative","resultsPLTSPHR3","1","true"});
+	}
+
+	@Test
+	public void testRun2()
+	{
+		TSPCachingTester.main(new String[] {"run","TSP1KParametersRelative","resultsTSPHR3","1","true"});
+	}
+
+
+}
