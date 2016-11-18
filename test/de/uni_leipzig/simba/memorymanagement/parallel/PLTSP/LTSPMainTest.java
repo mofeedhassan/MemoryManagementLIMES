ee1dcc81e9f8a1af189140900b491db5a48176fe
mofeedhassan <mofeed_mohammed@yahoo.com>
2016-11-18 13:48:22 +0100

    fix the parameters running tests

--- test/de/uni_leipzig/simba/memorymanagement/parallel/PLTSP/LTSPMainTest.java
+++ test/de/uni_leipzig/simba/memorymanagement/parallel/PLTSP/LTSPMainTest.java
@@ -8,17 +8,16 @@ import de.uni_leipzig.simba.memorymanagement.testTSPCaching.TSPCachingTester;
 
 public class LTSPMainTest {
 
-	@Test
+/*	@Test
 	public void testRun1()
 	{
 		LTSPMain.main(new String[] {"run","PLTSP1KParametersRelative","resultsPLTSPHR3","1","true"});
-	}
+	}*/
 
 	@Test
 	public void testRun2()
 	{
 		TSPCachingTester.main(new String[] {"run","TSP1KParametersRelative","resultsTSPHR3","1","true"});
 	}
-
 
 }
